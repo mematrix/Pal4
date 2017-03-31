4f20426a7be1785d63f0aa99cd92b25f4edd83f5
yuki <839259646@qq.com>
2017-04-01 01:24:20 +0800

    implementing battle loop...

--- Source/PAL4/BattleSystem/FBattleSystem.cpp
+++ Source/PAL4/BattleSystem/FBattleSystem.cpp
@@ -5,17 +5,38 @@
 #include <algorithm>
 
 #include "FBattleSystem.h"
+#include "ISupportRoundAction.h"
 #include "Character/FCharacterPropertyManager.h"
 
 
 FBattleSystem::FBattleSystem(TArray<TSharedRef<IBattleStatus>>& characters, TSharedRef<ICharacterRoundDispatcher>& dispatcher) :
+    BattleBeginEvent(),
+    BattleFinishedEvent(),
+    CharacterWillActEvent(),
+    CharacterFinishActEvent(),
     Characters(characters),
-    Dispatcher(dispatcher)
+    Dispatcher(dispatcher),
+    RoundManagers(),
+    CharacterActLast(nullptr)
 {
+    int32 count = Characters.Num();
+    RoundManagers.Reserve(count);
+    for (int i = 0; i < count; ++i)
+    {
+        auto& roundAction = Characters[i]->GetRoundAction();
+        RoundManagers.Add(MakeShared<FCharacterRoundManager>(roundAction));
+    }
 }
 
 FBattleSystem::~FBattleSystem()
 {
+}
+
+void FBattleSystem::AddCharacter(const TSharedRef<IBattleStatus>& character)
+{
+    auto& roundManager = MakeShared<FCharacterRoundManager>(character->GetRoundAction());
+    RoundManagers.Add(MoveTemp(roundManager));
+    Characters.Add(character);
 }
 
 void FBattleSystem::Run()
@@ -31,10 +52,28 @@ void FBattleSystem::Run()
         _ASSERT(0);
     }
 
+    IBattleStatus* characterActLast = nullptr;
     while (!BattleIsOver())
     {
-        auto& character = Dispatcher->MoveToNext(Characters);
-        //character
+        auto& character = Dispatcher->MoveToNext(Characters).Get();
+        characterActLast = &character;
+        if (CharacterWillActEvent.IsBound())
+        {
+            CharacterWillActEvent.Broadcast(*this, character);
+        }
+
+        auto roundManager = character.GetRoundAction().GetRoundManager();
+        roundManager->DoRoundAction();
+        if (CharacterFinishActEvent.IsBound())
+        {
+            CharacterFinishActEvent.Broadcast(*this, character);
+        }
+    }
+    CharacterActLast = characterActLast;
+
+    if (BattleFinishedEvent.IsBound())
+    {
+        BattleFinishedEvent.Broadcast(*this);
     }
 }
 
@@ -56,6 +95,39 @@ bool FBattleSystem::BattleIsOver() const
     }
 
     return !(isAnyPlayerAlive && isAnyNonPlayerAlive);
+}
+
+bool FBattleSystem::IsPlayerWinned() const
+{
+    if (nullptr == CharacterActLast)
+    {
+        return false;
+    }
+
+    bool isAnyPlayerAlive = false;
+    bool isAnyNonPlayerAlive = false;
+    for (int i = 0; i < Characters.Num(); ++i)
+    {
+        auto& manager = Characters[i]->GetPropertyManager();
+        if (manager.IsPlayer())
+        {
+            isAnyPlayerAlive = isAnyPlayerAlive || manager.IsAlive();
+        }
+        else
+        {
+            isAnyNonPlayerAlive = isAnyNonPlayerAlive || manager.IsAlive();
+        }
+    }
+
+    if (isAnyPlayerAlive && !isAnyNonPlayerAlive)
+    {
+        return true;
+    }
+    else if (isAnyNonPlayerAlive && !isAnyPlayerAlive)
+    {
+        return false;
+    }
+    return CharacterActLast->GetPropertyManager().IsPlayer();
 }
 
 bool FBattleSystem::IsPlayerWinned(TSharedRef<IBattleStatus>& lastCharacter) const
--- Source/PAL4/BattleSystem/FBattleSystem.h
+++ Source/PAL4/BattleSystem/FBattleSystem.h
@@ -7,6 +7,7 @@
 
 #include "IBattleStatus.h"
 #include "ICharacterRoundDispatcher.h"
+#include "FCharacterRoundManager.h"
 
 /**
  * 
@@ -14,8 +15,10 @@
 class PAL4_API FBattleSystem
 {
 public:
-    DECLARE_EVENT_OneParam(FBattleSystem, FBattleBeginEvent, const FBattleSystem&);
-    DECLARE_EVENT_OneParam(FBattleSystem, FBattleFinishedEvent, const FBattleSystem&);
+    DECLARE_EVENT_OneParam(FBattleSystem, FBattleBeginEvent, const FBattleSystem&)
+    DECLARE_EVENT_OneParam(FBattleSystem, FBattleFinishedEvent, const FBattleSystem&)
+    DECLARE_EVENT_TwoParams(FBattleSystem, FCharacterWillActEvent, const FBattleSystem&, const IBattleStatus&)
+    DECLARE_EVENT_TwoParams(FBattleSystem, FCharacterFinishActEvent, const FBattleSystem&, const IBattleStatus&)
 
 public:
     FBattleSystem(TArray<TSharedRef<IBattleStatus>>&, TSharedRef<ICharacterRoundDispatcher>&);
@@ -24,16 +27,17 @@ public:
 
     FBattleSystem& operator=(const FBattleSystem&) = delete;
 
+    // 战斗开始事件
     FBattleBeginEvent& OnBattleBegin() { return BattleBeginEvent; }
+    // 战斗结束事件
     FBattleFinishedEvent& OnBattleFinished() { return BattleFinishedEvent; }
+    FCharacterWillActEvent& OnCharacterWillAct() { return CharacterWillActEvent; }
+    FCharacterFinishActEvent& OnCharacterFinishAct() { return CharacterFinishActEvent; }
 
     // TArray<TSharedRef<IBattleStatus>>& GetCharacters() { return Characters; }
     const TArray<TSharedRef<IBattleStatus>>& GetCharacters() const { return Characters; }
 
-    void AddCharacter(const TSharedRef<IBattleStatus>& character)
-    {
-        Characters.Add(character);
-    }
+    void AddCharacter(const TSharedRef<IBattleStatus>& character);
 
     const TSharedRef<ICharacterRoundDispatcher>& GetDispatcher() const { return Dispatcher; }
 
@@ -47,14 +51,29 @@ public:
      */
     void Run();
 
-private:
     bool BattleIsOver() const;
+    /*
+     * 获取战斗结束后玩家一方是否胜利。
+     * @warning 此方法仅在战斗结束(@see BattleIsOver)后才具有正确的返回值，如果战斗未结束，返回值是未知的。
+     */
+    bool IsPlayerWinned() const;
+
+private:
     bool IsPlayerWinned(TSharedRef<IBattleStatus>&) const;
 
 private:
     FBattleBeginEvent BattleBeginEvent;
     FBattleFinishedEvent BattleFinishedEvent;
+    FCharacterWillActEvent CharacterWillActEvent;
+    FCharacterFinishActEvent CharacterFinishActEvent;
 
     TArray<TSharedRef<IBattleStatus>> Characters;
     TSharedRef<ICharacterRoundDispatcher> Dispatcher;
+    // 存储每个参战人物的回合管理器实例。不直接存对象而是存储智能指针，原因是这个列表可能会有增加操作，
+    // 那么就有可能会导致列表存储内存重分配，造成指向其中元素实例的指针悬挂（在FCharacterRoundManager里
+    // 面会用到自身的this指针的值。）
+    TArray<TSharedRef<FCharacterRoundManager>> RoundManagers;
+
+    // 最后一次出手的人
+    IBattleStatus *CharacterActLast;
 };
