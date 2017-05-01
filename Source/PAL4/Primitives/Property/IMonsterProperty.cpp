6fb07edb50a5a1c3ee98ae88ee3c33f1eda3b0c8
yuki <839259646@qq.com>
2017-05-02 00:48:27 +0800

    add monster property base class.

---
+++ Source/PAL4/Primitives/Property/IMonsterProperty.cpp
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PAL4.h"
+
+#include "IMonsterProperty.h"
+#include "../Model/FMonsterInherentInfo.h"
+
+
+IMonsterProperty::IMonsterProperty() :
+    ICharacterProperty(),
+    ImmuneInfo{ 0 }
+{
+}
+
+bool IMonsterProperty::IsDebuffImmune(EImmune m)
+{
+    return (GetMonsterInherentInfo().ImmuneFlags | ImmuneInfo.ImmuneFlags) & static_cast<int32>(m);
+}
---
+++ Source/PAL4/Primitives/Property/IMonsterProperty.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "ICharacterProperty.h"
+#include "../Model/FMonsterImmuneInfo.h"
+
+struct FMonsterInherentInfo;
+
+
+class PAL4_API IMonsterProperty : public ICharacterProperty
+{
+protected:
+    IMonsterProperty();
+    IMonsterProperty(const IMonsterProperty&) = default;
+    IMonsterProperty(IMonsterProperty&&) = default;
+    
+    IMonsterProperty& operator=(const IMonsterProperty&) = default;
+    IMonsterProperty& operator=(IMonsterProperty&&) = default;
+
+public:
+    bool IsDebuffImmune(EImmune) override final;
+
+    FMonsterImmuneInfo& AttachedImmuneInfo() { return ImmuneInfo; }
+    const FMonsterImmuneInfo& AttachedImmuneInfo() const { return ImmuneInfo; }
+
+    virtual const FMonsterInherentInfo& GetMonsterInherentInfo() const = 0;
+
+private:
+    FMonsterImmuneInfo ImmuneInfo;
+};
