362a06b6d893c722cfcd14ac90888e535ba840ef
yuki <839259646@qq.com>
2017-05-23 01:02:51 +0800

    add skill executor utility. supplement interfaces of `ISkill`.

---
+++ Source/PAL4/Skill/FSkillExecutor.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "ISkill.h"
+
+
+/**
+ * 一个工具类，用于执行一次技能，包括这个技能附带的所有执行任务。附加任务常见于附加技能、引发的其它技能等；
+ * 比如@code ESkillTriggerType \endcode 中描述的触发任务。
+ */
+class PAL4_API FSkillExecutor
+{
+    //
+};
--- Source/PAL4/Skill/ISkill.h
+++ Source/PAL4/Skill/ISkill.h
@@ -2,21 +2,32 @@
 
 #include <functional>
 #include <map>
-#include <vector>
 
-#include <Platform.h>
+#include "FSkillResult.h"
 
 class ICharacterCombatDelegate;
 
 
+/**
+ * 技能基类。定义了一个技能执行的基本过程
+ */
 class PAL4_API ISkill
 {
 public:
     virtual ~ISkill() = default;
 
+    /**
+     * 计算技能执行预期结果。此结果将会继续由后续管道过程修正
+     */
+    virtual void ComputeResult(std::map<std::reference_wrapper<ICharacterCombatDelegate>, FSkillResult>&) = 0;
+
     virtual void BeforeAction() = 0;
 
-    // TODO: int32需要替换为伤害数据结构
-    virtual void DoAction(const std::map<std::reference_wrapper<ICharacterCombatDelegate>, std::vector<int32>>&,
-        const std::function<void(ICharacterCombatDelegate&, ICharacterCombatDelegate&, int32)>&) = 0;
+    /**
+     * 执行技能过程。传入一个回调函数，此函数接受三个参数：攻击者、被攻击对象以及伤害模型对象
+     */
+    virtual void DoAction(const std::map<std::reference_wrapper<ICharacterCombatDelegate>, FSkillResult>&,
+        const std::function<void(ICharacterCombatDelegate&, ICharacterCombatDelegate&, const FSkillResult&)>&) = 0;
+
+    virtual void AfterAction() = 0;
 };
