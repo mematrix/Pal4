a183c76e49d2484dc8134a75a8a4fe5b3a107d2f
yuki <839259646@qq.com>
2017-06-02 00:46:20 +0800

    add complete and independent skill trigger definition

--- Source/PAL4/Skill/Enum/ESkillTriggerType.h
+++ Source/PAL4/Skill/Enum/ESkillTriggerType.h
@@ -12,9 +12,9 @@ enum class PAL4_API ESkillTriggerType
     EffectAnyTime,
     // 调用
     Invoke,
-    // 被普通攻击击中
+    // 被普通攻击击中，对自身
     HitByPhysicalAttack,
-    // 普通攻击击中目标后触发
+    // 普通攻击击中目标后触发，对目标
     HitTarget,
     // 同伴数量减少
     CompanionsReduced,
@@ -32,8 +32,42 @@ enum class PAL4_API ESkillTriggerType
     HitBySoilMagic,
     // 被命中后，对攻击方
     BeHitThenToActor,
-    // 击中目标后，对自己
+    // 普通攻击击中目标后，对自己
     HitTargetThenToSelf,
     // 普通攻击行动结束时，对自己
     PhysicalAttackFinishThenToSelf
+};
+
+
+enum class PAL4_API ESkillAttackType
+{
+    // 物理攻击
+    Physical,
+    // 水系仙术攻击
+    WaterMagic,
+    // 火系仙术攻击
+    FireMagic,
+    // 雷系仙术攻击
+    ThunderMagic,
+    // 风系仙术攻击
+    WindMagic,
+    // 土系仙术攻击
+    SoilMagic,
+    // 特技攻击
+    Stunt,
+    // 道具攻击
+    Prop
+};
+
+
+enum class PAL4_API ESkillTriggerTarget
+{
+    // 攻击者触发，对自身
+    ActorToSelf,
+    // 攻击者触发，对被攻击目标
+    ActorToPeer,
+    // 被攻击者触发，对自身
+    TargetToSelf,
+    // 被攻击者触发，对攻击者
+    TargetToPeer
 };
