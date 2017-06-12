1062f28fea3fb8b1f5f9bd752fc4c323e7353a28
yuki <839259646@qq.com>
2017-06-13 00:09:35 +0800

    implementing main operations.

--- Source/PAL4/Combat/Implement/Character/FCombatCharacter.cpp
+++ Source/PAL4/Combat/Implement/Character/FCombatCharacter.cpp
@@ -47,56 +47,187 @@ void FCombatCharacter::SetStatusTransform(int32 skillID, ECharacterStatusType ty
 
 void FCombatCharacter::ClearSkillStatus(int32 skillID)
 {
+    TempStatus.RemoveTransformerByKey(skillID);
+
+    SkillRoundRecords.remove_if([skillID, this](const FSkillRoundRecord& record) -> bool
+    {
+        if (skillID == record.SkillID)
+        {
+            RoundManager.RemoveDelayCallFunc(record.RoundFuncKey);
+            return true;
+        }
+
+        return false;
+    });
 }
 
 void FCombatCharacter::ClearSpecialStatus(ECharacterStatusType type)
 {
+    TempStatus.RemoveTransformerByStatus(type);
+
+    SkillRoundRecords.remove_if([type, this](const FSkillRoundRecord& record) -> bool {
+        if (type == record.Type)
+        {
+            RoundManager.RemoveDelayCallFunc(record.RoundFuncKey);
+            return true;
+        }
+
+        return false;
+    });
 }
 
 void FCombatCharacter::ClearStatus(int32 skillID, ECharacterStatusType type)
 {
+    TempStatus.RemoveTransformer(skillID, type);
+
+    SkillRoundRecords.remove_if([skillID, type, this](const FSkillRoundRecord& record) -> bool {
+        if (skillID == record.SkillID && type == record.Type)
+        {
+            RoundManager.RemoveDelayCallFunc(record.RoundFuncKey);
+            return true;
+        }
+
+        return false;
+    });
 }
 
 void FCombatCharacter::SetBuff(EBuff buff, uint32 maxRoundNum)
 {
+    if (EBuff::None != TempStatus.GetBuffStatus())
+    {
+        RoundManager.RemoveDelayCallFunc(RoundFuncKeyBuff);
+    }
+
+    TempStatus.SetBuffStatus(buff);
+    // TODO: 根据Buff值，添加对应的被动触发技能、以及修正伤害结果的函数
+    if (EBuff::None != buff)
+    {
+        RoundFuncKeyBuff = RoundManager.AddDelayCallFunc(maxRoundNum, false, [this] {
+            this->SetBuff(EBuff::None, 0);
+        });
+    }
 }
 
 void FCombatCharacter::SetPoison(EPoison poison, uint32 maxRoundNum)
 {
+    if (EPoison::None != TempStatus.GetPoisonStatus())
+    {
+        RoundManager.RemoveDelayCallFunc(RoundFuncKeyPoison);
+    }
+
+    TempStatus.SetPoisonStatus(poison);
+    if (EPoison::None != poison)
+    {
+        RoundFuncKeyPoison = RoundManager.AddDelayCallFunc(maxRoundNum, false, [this] {
+            this->SetPoison(EPoison::None, 0);
+        });
+    }
 }
 
 void FCombatCharacter::SetDebuff(EDebuff debuff, uint32 maxRoundNum)
 {
+    if (EDebuff::None != TempStatus.GetDebuffStatus())
+    {
+        RoundManager.RemoveDelayCallFunc(RoundFuncKeyDebuff);
+    }
+
+    TempStatus.SetDebuffStatus(debuff);
+    if (EDebuff::None != debuff)
+    {
+        RoundFuncKeyDebuff = RoundManager.AddDelayCallFunc(maxRoundNum, false, [this] {
+            this->SetDebuff(EDebuff::None, 0);
+        });
+    }
 }
 
 void FCombatCharacter::SetResurrect(bool canResurrct, uint32 maxRoundNum)
 {
+    if (TempStatus.CanResurrect())
+    {
+        RoundManager.RemoveDelayCallFunc(RoundFuncKeyResurrect);
+    }
+
+    TempStatus.SetResurrectStatus(canResurrct);
+    if (canResurrct)
+    {
+        RoundFuncKeyResurrect = RoundManager.AddDelayCallFunc(maxRoundNum, false, [this] {
+            this->SetResurrect(false, 0);
+        });
+    }
 }
 
 void FCombatCharacter::SetInvisible(bool invisible, uint32 maxRoundNum)
 {
+    if (TempStatus.IsInvisible())
+    {
+        RoundManager.RemoveDelayCallFunc(RoundFuncKeyInvisible);
+    }
+
+    TempStatus.SetInvisibleStatus(invisible);
+    if (invisible)
+    {
+        RoundFuncKeyInvisible = RoundManager.AddDelayCallFunc(maxRoundNum, false, [this] {
+            this->SetInvisible(false, 0);
+        });
+    }
 }
 
-void FCombatCharacter::AmendResult(FSkillResult&, const ISkill&)
+void FCombatCharacter::AmendResult(FSkillResult& result, const ISkill& skill)
 {
+    for (const auto& func : AmendFuncs)
+    {
+        if (func)
+        {
+            func(result, skill);
+        }
+    }
 }
 
-void FCombatCharacter::OnBasicSkillFinished(const FBasicInfoResultRecord&)
+void FCombatCharacter::OnBasicSkillFinished(const FBasicInfoResultRecord& record)
 {
+    for (const auto& func : BasicInfoFinishedFuncs)
+    {
+        if (func)
+        {
+            func(record);
+        }
+    }
 }
 
-void FCombatCharacter::OnStatusSkillFinished(const FStatusInfoResultRecord&)
+void FCombatCharacter::OnStatusSkillFinished(const FStatusInfoResultRecord& record)
 {
+    for (const auto& func : StatusInfoFinishedFuncs)
+    {
+        if (func)
+        {
+            func(record);
+        }
+    }
 }
 
-void FCombatCharacter::OnTriggerSkillFinished(const FTriggerResultRecord&)
+void FCombatCharacter::OnTriggerSkillFinished(const FTriggerResultRecord& record)
 {
+    for (const auto& func : TriggerFinishedFuncs)
+    {
+        if (func)
+        {
+            func(record);
+        }
+    }
 }
 
-void FCombatCharacter::OnCombatStatusSkillFinished(const FCombatStatusResultRecord&)
+void FCombatCharacter::OnCombatStatusSkillFinished(const FCombatStatusResultRecord& record)
 {
+    for (const auto& func : CombatStatusFinishedFuncs)
+    {
+        if (func)
+        {
+            func(record);
+        }
+    }
 }
 
-void FCombatCharacter::TriggerSkill(const FSkillTriggerInfo&)
+void FCombatCharacter::TriggerSkill(const FSkillTriggerInfo& info)
 {
+
 }
