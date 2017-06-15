63db9539470a4da57daa90fd8450c23351714982
yuki <839259646@qq.com>
2017-06-16 00:10:47 +0800

    add declaration of func to create trigger skill.

--- Source/PAL4/Skill/FSkillFactory.h
+++ Source/PAL4/Skill/FSkillFactory.h
@@ -1,1 +1,13 @@
 #pragma once
+
+#include <SharedPointer.h>
+
+class ISkill;
+class ICharacterDelegate;
+
+
+class PAL4_API FSkillFactory
+{
+public:
+    TSharedPtr<ISkill> CreateTriggerSkill(int32 skillID, ICharacterDelegate* actor, ICharacterDelegate& target);
+};
